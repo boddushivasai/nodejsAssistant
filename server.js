17a1fe05c63963fa4d8e4d760ff98cdadbdbed51
boddu shivasai <shivasai@mightyapps.ai>
2025-03-13 16:58:22 +0530

    Add/Update server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+ 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let data = [];
+
+app.post('/create', (req, res) => {
+    const item = req.body;
+    data.push(item);
+    res.send({ message: 'Item created successfully', data });
+});
+
+app.get('/read', (req, res) => {
+    res.send({ data });
+});
+
+app.put('/update/:id', (req, res) => {
+    const id = req.params.id;
+    const item = req.body;
+
+    data[id] = item;
+    res.send({ message: 'Item updated successfully', data });
+});
+
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+
+    data = data.filter((item, index) => index != id);
+    res.send({ message: 'Item deleted successfully', data });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
