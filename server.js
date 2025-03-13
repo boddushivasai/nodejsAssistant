4c649485ef9bde691d57fd63d5d70a61ee7e4c7d
boddu shivasai <shivasai@mightyapps.ai>
2025-03-13 17:05:02 +0530

    made mistake app to ap
    so if we run this code so we get error.. app is not defied

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
+const ap = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
